1b172361829dd21311d6d50f81bb2e824af9f786
can <can@ASUS-N50V>
2019-08-19 01:31:24 +0300

    Güneş ŞAHİN

---
+++ assets/theme/js/app.js
@@ -0,0 +1,48 @@
+﻿/*
+    ████████████████████████████████████████████████████████████████████████████████████████████████████
+    * Application: web template v0.0.0.1
+    * https://gunessahin.github.io/
+    ════════════════════════════════════════════════════════════════════════════════════════════════════
+    * Copyright gunessahin@outlook.com.tr
+    * Licence (https://github.com/gunessahin)
+    ████████████████████████████████████████████████████████████████████████████████████████████████████
+*/
+console.log("Application Start");
+
+/**
+ * Sayfa içerisine belgeyi yükler.
+ * Belge yüklenmeden önca çaliştırırlır.
+ * @param {any} page Yüklenecek, çalıştırılacak belge
+ * @param {any} container Yüklenecek tayıyıcı element
+ * @param {any} callback Yükleme tamamlandığında çalışacak foksiyon
+ */
+function RenderPartial(page, container, callback) {
+    $(container).load(page, callback);
+}
+
+/**
+ * Modalı Göster
+ * @param {any} element Modal Element Adı
+ */
+function ShowModal(element) {
+    $(element).modal('show');
+}
+
+/**
+ * Sayfayı Yükle Çalıştır ve Göster
+ * @param {any} page Yüklenecek, çalıştırılacak belge
+ * @param {any} container Yüklenecek tayıyıcı element
+ * @param {any} element Modal Element Adı
+ */
+function ShowPage(page, container, element) {
+
+    // Belgeyi Sayfa İçine Yükle
+    RenderPartial(page, container,
+
+        // Sayfa Yüklendi
+        function () {
+
+            // Modalı Göster
+            ShowModal(element);
+        });
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Web Template
+
+Web Template; 
+The all-in-one, user interface kit.
+This template comes with ready-to-use examples in many programming languages.
+HTML, ASP, ASP-NET, PHP, JAVA, NODEJS, PYTHON
+
+by Gunes SAHIN. is licensed
+
+Based on a work at https://gunessahin.github.io/
+
+## Licence
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+    <img alt="Creative Commons License" style="border-width:0"
+        src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" />
+</a>
+
+<span xmlns:dct="https://purl.org/dc/terms/" href="https://purl.org/dc/dcmitype/InteractiveResource" property="dct:title"
+    rel="dct:type">Web Template</span> by <a xmlns:cc="https://creativecommons.org/ns#"
+    href="https://gunessahin.github.io/" property="cc:attributionName" rel="cc:attributionURL">Gunes SAHIN</a> is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+
+Based on a work at <a xmlns:dct="https://purl.org/dc/terms/" href="https://gunessahin.github.io/" rel="dct:source">https://gunessahin.github.io/</a>
